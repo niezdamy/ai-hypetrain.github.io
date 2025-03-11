cbf01569a39b9e946f7db1ce7ef23341092dd8d7
Oskar <oskar.poprawski@hotmail.com>
2025-03-11 23:13:25 +0100

    Trailing slash added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+// Add the trailingSlash configuration to ensure proper routing
 module.exports = {
   basePath: '/ai-hypetrain.github.io',
   i18n: {
     locales: ['en', 'pl'],
     defaultLocale: 'en'
-  }
+  },
+  trailingSlash: true
 };
